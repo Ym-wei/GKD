7739df553f12f5bb5e25fdd43a4be74269861d31
杨明炜 <285133472@qq.com>
2024-04-03 10:22:33 +0800

    feat: 31

--- src/apps/com.xiaomi.smarthome.ts
+++ src/apps/com.xiaomi.smarthome.ts
@@ -13,7 +13,7 @@ export default defineAppConfig({
         {
           quickFind: true,
           activityIds: 'com.xiaomi.smarthome.SmartHomeMainActivity',
-          matches: '[id="com.xiaomi.smarthome:id/ali"][visibleToUser=true]',
+          matches: '[text="进入"][visibleToUser=true]',
           exampleUrls:
             'https://m.gkd.li/81805625/50995a63-ded4-42fc-a642-8c35206e8dd4',
           snapshotUrls: 'https://i.gkd.li/i/14413607',
